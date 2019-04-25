760cb6a990b498b9352dfb19db641b1f681cc794
Anamul_ru <roni.twenty@gmail.com>
2019-04-25 17:01:48 +0600

    django bloq last commit

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,50 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView , CreateView, UpdateView ,DeleteView
 from .models import Post
-
-posts = [
-     {
-        'author':'CoreyMS',
-        'title': 'Blog Post 1',
-        'content':'First post content',
-        'date_posted':'August 27, 2018'
-     },
-      {
-         'author':'jon',
-         'title': 'Blog Post 2',
-         'content':'Second post content',
-         'date_posted':'August 28, 2018'
-      }
-
-]
-
+from django.contrib.auth.mixins import LoginRequiredMixin , UserPassesTestMixin
 def home(request):
     context = {
         'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def About(request):
     return render(request, 'blog/about.html',{'title':'About'})
